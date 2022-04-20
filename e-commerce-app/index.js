a8aa00e94ddd6b46b19f5cbd0a72832fe85e510a
jsprofessional <jshuman@protonmail.com>
2022-04-20 07:57:27 -0500

    Coded along through s2e7

--- e-commerce-app/index.js
+++ e-commerce-app/index.js
@@ -1,18 +1,33 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+// Scalar types: String, Int, Float, Boolean, ID
+// ! makes a type non-nullable
+
 const typeDefs = gql`
 	type Query {
-		hello: String
+		hello: String!
+		numberOfAnimals: Int
+		price: Float
+		isCool: Boolean
 	}
 `;
 
 const resolvers = {
 	Query: {
 		hello: () => {
-			return "World!!!!!!YEET"
+			return null;
+		},
+		numberOfAnimals: () => {
+			return 55;
+		},
+		price: () => {
+			return 2345.5432;
+		},
+		isCool: () => {
+			return false;
 		}
-	}
-}
+	},
+};
 
 const server = new ApolloServer({
 	typeDefs,
